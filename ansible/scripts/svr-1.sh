a923f849465f1dd0c6c002a7e2e42a31107641b6
unknown <116912828+rerichardjr@users.noreply.github.com>
2024-05-13 13:49:00 -0500

    add ansible user

--- ansible/scripts/svr-1.sh
+++ ansible/scripts/svr-1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 set -euxo pipefail
+
+# create ansible user
+sudo useradd ansible -G sudo -m -s /bin/bash
+RANDOM_PW=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo)
+echo "ansible:$RANDOM_PW" | sudo chpasswd
+
+echo '##############################################################'
+echo 'ansible user password is ' $RANDOM_PW
