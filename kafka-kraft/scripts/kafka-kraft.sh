9362626a8326ea064b0212979517feb272299ae6
Robby Richard <116912828+rerichardjr@users.noreply.github.com>
2024-08-05 15:31:25 -0500

    Update kafka-kraft.sh

--- kafka-kraft/scripts/kafka-kraft.sh
+++ kafka-kraft/scripts/kafka-kraft.sh
@@ -3,11 +3,10 @@
 set -euxo pipefail
 
 KAFKA_INSTALLER=kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
-KAFKA_SERVER_PROPERTIES=/opt/kafka/config/kraft/server.properties
+KAFKA_SERVER_PROPERTIES=${INSTALL_FOLDER}/config/kraft/server.properties
 KAFKA_CLUSTER_ID_FILE=/vagrant/kafka_cluster_id.txt
 KAFKA_PASSWORD_FILE=/vagrant/password.txt
 KAFKA_SERVICE=/etc/systemd/system/kafka.service
-
 
 # install java jdk
 wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
@@ -20,8 +19,8 @@ for i in `seq 1 ${NODE_COUNT}`; do
     echo "$NETWORK$((HOST_START+i)) node${i}.$DOMAIN" >> /etc/hosts
 done
 
-# create kafka user
-sudo useradd kafka -G sudo -m -s /bin/bash
+# create user
+sudo useradd ${RUN_AS_USER} -G sudo -m -s /bin/bash
 if [ ! -f $KAFKA_PASSWORD_FILE ]; then
   RANDOM_PW=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo)
   echo $RANDOM_PW > $KAFKA_PASSWORD_FILE
@@ -29,21 +28,21 @@ else
   RANDOM_PW=$(cat $KAFKA_PASSWORD_FILE)
 fi
 
-echo "kafka:$RANDOM_PW" | sudo chpasswd
+echo "$RUN_AS_USER:$RANDOM_PW" | sudo chpasswd
 
 # download kafka if not already staged
 if [ ! -f /tmp/$KAFKA_INSTALLER ]; then
   echo "Kafka installer not found"
-  sudo -u kafka wget https://downloads.apache.org/kafka/${KAFKA_VERSION}/$KAFKA_INSTALLER -O /tmp/$KAFKA_INSTALLER
+  sudo -u ${RUN_AS_USER} wget https://downloads.apache.org/kafka/${KAFKA_VERSION}/$KAFKA_INSTALLER -O /tmp/$KAFKA_INSTALLER
 fi
 
 # install kafka
-sudo mkdir /opt/kafka
-sudo mkdir /var/log/kafka
-sudo chown kafka:kafka /opt/kafka /var/log/kafka
-sudo -u kafka tar xzf /tmp/$KAFKA_INSTALLER -C /opt/kafka --strip 1
+sudo mkdir ${INSTALL_FOLDER}
+sudo mkdir ${LOG_FOLDER}
+sudo chown ${RUN_AS_USER}:${RUN_AS_USER} ${INSTALL_FOLDER} ${LOG_FOLDER}
+sudo -u ${RUN_AS_USER} tar xzf /tmp/$KAFKA_INSTALLER -C ${INSTALL_FOLDER} --strip 1
 
-# create kafka server.properties in /opt/kafka/config/kraft
+# create kafka server.properties in ${INSTALL_FOLDER}/config/kraft
 # build list of hosts for the controller.quorum.voters configuration parameter
 for i in `seq 1 ${NODE_COUNT}`; do
   if [ $i -eq 1 ]; then
@@ -53,9 +52,9 @@ for i in `seq 1 ${NODE_COUNT}`; do
   fi
 done
 
-sudo -u kafka mv $KAFKA_SERVER_PROPERTIES $KAFKA_SERVER_PROPERTIES.old
+sudo -u ${RUN_AS_USER} mv $KAFKA_SERVER_PROPERTIES $KAFKA_SERVER_PROPERTIES.orig
 if [ ! -f $KAFKA_SERVER_PROPERTIES ]; then
-  sudo -u kafka cat > $KAFKA_SERVER_PROPERTIES <<EOF
+  sudo -u ${RUN_AS_USER} cat > $KAFKA_SERVER_PROPERTIES <<EOF
 process.roles=broker,controller
 node.id=$NODE_ID
 controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS
@@ -64,15 +63,16 @@ inter.broker.listener.name=PLAINTEXT
 advertised.listeners=PLAINTEXT://$HOSTNAME.$DOMAIN:9092
 controller.listener.names=CONTROLLER
 listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
-log.dirs=/var/log/kafka/kraft-combined-logs
-num.partitions=$((NODE_COUNT*2))
+log.dirs=${LOG_FOLDER}/kraft-combined-logs
+num.partitions=3
+auto.create.topics.enable=false
 EOF
 fi
 
 # generate id for cluster
 if [ $HOSTNAME == "node1" ]; then
   # if node1, create cluster id and save id so other vms can access
-  sudo -u kafka /opt/kafka/bin/kafka-storage.sh random-uuid > $KAFKA_CLUSTER_ID_FILE 
+  sudo -u ${RUN_AS_USER} ${INSTALL_FOLDER}/bin/kafka-storage.sh random-uuid > $KAFKA_CLUSTER_ID_FILE 
   KAFKA_CLUSTER_ID=$(cat $KAFKA_CLUSTER_ID_FILE)
 else
   # if any other node, get cluster id from cluster file in vagrant folder
@@ -80,7 +80,7 @@ else
 fi
 
 # format storage dir
-sudo -u kafka /opt/kafka/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /opt/kafka/config/kraft/server.properties
+sudo -u ${RUN_AS_USER} ${INSTALL_FOLDER}/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c ${INSTALL_FOLDER}/config/kraft/server.properties
 
 # create kafka service
 if [ ! -f $KAFKA_SERVICE ]; then
@@ -95,8 +95,8 @@ StartLimitBurst=25
 [Service]
 Type=simple
 User=kafka 
-ExecStart=/bin/sh -c '/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/kraft/server.properties > /var/log/kafka/kafka.log 2>&1'
-ExecStop=/opt/kafka/bin/kafka-server-stop.sh
+ExecStart=/bin/sh -c '${INSTALL_FOLDER}/bin/kafka-server-start.sh ${INSTALL_FOLDER}/config/kraft/server.properties > ${LOG_FOLDER}/kafka.log 2>&1'
+ExecStop=${INSTALL_FOLDER}/bin/kafka-server-stop.sh
 Restart=on-failure
 RestartSec=5s
 
@@ -109,6 +109,8 @@ fi
 sudo systemctl enable kafka
 sudo systemctl start kafka
 
+# add install folder path to vagrant users env
+echo 'export PATH="$PATH:'${INSTALL_FOLDER}'/bin"' >> .bashrc
 
 echo '##############################################################'
 echo 'kafka user password is '$RANDOM_PW
